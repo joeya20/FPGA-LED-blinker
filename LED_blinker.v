69cf15ecb501437447593208e5a59d037beefa2e
joeya20 <56373482+joeya20@users.noreply.github.com>
2021-12-30 21:24:50 -0700

    clean up

--- LED_blinker.v
+++ LED_blinker.v
@@ -1,7 +1,9 @@
-//Design HDL code that will blink an LED at a specified frequency of 100 Hz, 50 Hz, 10 Hz, or 1 Hz. 
-//For each of the blink frequencies, the LED will be set to 50% duty cycle (it will be on half the time). 
-//The LED frequency will be chosen via two switches which are inputs to the FPGA. 
-//There is an additional switch called LED_EN that needs to be ‘1’ to turn on the LED. The FPGA will be driven by a 25 MHz oscillator.
+/*
+Design HDL code that will blink LEDs at a specified frequency of 100 Hz, 50 Hz, 10 Hz, or 1 Hz. 
+For each of the blink frequencies, the LED will be set to 50% duty cycle (it will be on half the time). 
+The LED frequency will be chosen via two switches which are inputs to the FPGA. 
+There is an additional switch that needs to be ‘1’ to turn on the LED. The FPGA will be driven by a 50MHz clock.
+*/
 
 module LED_blinker 
 	(
@@ -58,8 +60,8 @@ module LED_blinker
 		begin
 			if(r_count_50hz == c_max_count_50Hz-1)
 				begin
-				r_toggle_50Hz <= !r_toggle_50Hz;
-				r_count_50hz <= 0;
+					r_toggle_50Hz <= !r_toggle_50Hz;
+					r_count_50hz <= 0;
 				end
 			else
 				r_count_50hz <= r_count_50hz + 1;
@@ -70,8 +72,8 @@ module LED_blinker
 		begin
 			if(r_count_10hz == c_max_count_10Hz-1)
 				begin
-				r_toggle_10Hz <= !r_toggle_10Hz;
-				r_count_10hz <= 0;
+					r_toggle_10Hz <= !r_toggle_10Hz;
+					r_count_10hz <= 0;
 				end
 			else
 				r_count_10hz <= r_count_10hz + 1;
@@ -82,8 +84,8 @@ module LED_blinker
 		begin
 			if(r_count_1hz == c_max_count_1Hz-1)
 				begin
-				r_toggle_1Hz <= !r_toggle_1Hz;
-				r_count_1hz <= 0;
+					r_toggle_1Hz <= !r_toggle_1Hz;
+					r_count_1hz <= 0;
 				end
 			else
 				r_count_1hz <= r_count_1hz + 1;
